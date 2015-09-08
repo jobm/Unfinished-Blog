acfdf5c86a47acf6ec392bbf246bb671b9d4da36
JobM <jobmwaniki18@gmail.com>
2015-09-08 11:22:00 +0300

    modified routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :comments
   resources :posts do
     resources :comments, :only => [:create]
@@ -7,8 +8,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
+root :to => "posts#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
